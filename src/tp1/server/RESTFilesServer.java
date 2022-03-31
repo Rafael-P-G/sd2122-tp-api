d8864e0f0e4a2ce8b342ba57389a156b99efcc97
Rafael <rafaac.ninja@gmail.com>
2022-03-31 01:11:48 +0100

    completed this class

--- src/tp1/server/RESTFilesServer.java
+++ src/tp1/server/RESTFilesServer.java
@@ -1,5 +1,14 @@
 package tp1.server;
 
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import tp1.server.resources.UsersResource;
+import tp1.server.util.GenericExceptionMapper;
+import util.Debug;
+
+import java.net.InetAddress;
+import java.net.URI;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RESTFilesServer {
@@ -17,6 +26,23 @@ public class RESTFilesServer {
     //TODO complete class 57956
 
     public static void main(String[] args) {
+        try {
+            Debug.setLogLevel( Level.INFO, Debug.SD2122 );
 
+            ResourceConfig config = new ResourceConfig();
+            config.register(UsersResource.class);
+            //config.register(CustomLoggingFilter.class);
+            config.register(GenericExceptionMapper.class);
+
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            String serverURI = String.format(SERVER_URI_FMT, ip, PORT);
+            JdkHttpServerFactory.createHttpServer( URI.create(serverURI), config);
+
+            Log.info(String.format("%s Server ready @ %s\n",  SERVICE, serverURI));
+
+            //More code can be executed here...
+        } catch( Exception e) {
+            Log.severe(e.getMessage());
+        }
     }
 }
