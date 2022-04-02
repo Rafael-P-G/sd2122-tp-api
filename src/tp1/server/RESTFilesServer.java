50615d9174bded00995e654dc1fcafee3886068d
Rafael <rafaac.ninja@gmail.com>
2022-04-02 22:52:20 +0100

    changes

--- src/tp1/server/RESTFilesServer.java
+++ src/tp1/server/RESTFilesServer.java
@@ -3,7 +3,7 @@ package tp1.server;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import tp1.server.discovery.Discovery;
-import tp1.server.resources.UsersResource;
+import tp1.server.resources.FilesResources;
 import tp1.server.util.GenericExceptionMapper;
 import util.Debug;
 
@@ -34,7 +34,7 @@ public class RESTFilesServer {
             Debug.setLogLevel( Level.INFO, Debug.SD2122 );
 
             ResourceConfig config = new ResourceConfig();
-            config.register(UsersResource.class);
+            config.register(FilesResources.class);
             //config.register(CustomLoggingFilter.class);
             config.register(GenericExceptionMapper.class);
 
